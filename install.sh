96358366c21ec087a6f25df4a52275a7b07bd0d7
Peter Finn <peter123finn@gmail.com>
2025-01-07 12:16:53 +0000

    Update install.sh
    Evilginx modifications added. Still need to sort out issue with kali repository adding...

--- install.sh
+++ install.sh
@@ -2,42 +2,46 @@
 logfile="installer.log"
 
 if [ $(id -u) -ne 0 ]; then 
-	echo "Run as root."
-	exit 1
+        echo "Run as root."
+        exit 1
 fi
 
 function runcmd {
-	echo "running command: \"$*\""
-	printf "\n\n\n=========================================\n" >> ${logfile}
-	echo "Output for command: $*" >> ${logfile}
-	$* >> ${logfile} 2>&1	
+        echo "running command: \"$*\""
+        printf "\n\n\n=========================================\n" >> ${logfile}
+        echo "Output for command: $*" >> ${logfile}
+        $* >> ${logfile} 2>&1
 }
 
 # These are necessary apt binaries
 function checkInstalled {
-	if [ -z "$(which $1)" ]; then
-		echo not installed: $1
-		runcmd "sudo apt -y install $2"
-	else
-		echo "$1 is installed"
-	fi
+        if [ -z "$(which $1)" ]; then
+                echo not installed: $1
+                runcmd "sudo apt -y install $2"
+        else
+                echo "$1 is installed"
+        fi
 }
 
 # These may already be installed somewhere else. Check with user.
 function askInstall {
-	echo "Install $1? y/n"
-	read opt
-	if [ "$opt" = "y" ]; then
-		echo "installing..."
-		runcmd "$2"
-	fi
+        echo "Install $1? y/n"
+        read opt
+        if [ "$opt" = "y" ]; then
+                echo "installing..."
+                runcmd "$2"
+        fi
 }
 
 # for API requests to Gophish
 function configureGophish {
-	API=$(sqlite3 gophish.db "SELECT api_key FROM users")
-	curl --insecure -X POST -H \"Content-Type: application/json\" -H \"Authorization: $API\" --data \"$2\" https://localhost:3333${1} -v
-#	runcmd "$cmd"
+        API=$(sqlite3 gophish.db "SELECT api_key FROM users")
+        curl --insecure -X POST -H \"Content-Type: application/json\" -H \"Authorization: $API\" --data \"$2\" https://localhost:3333${1} -v
+}
+
+function evilconfig {
+        printf "Sending the following commands to evilginx2:\n$* \n"
+        printf "$*" | evilginx2 > /dev/null
 }
 
 echo "Logs for all actions taken by this script are logged to ${logfile}"
@@ -45,7 +49,7 @@ echo "Logs for all actions taken by this script are logged to ${logfile}"
 # Add kali repos
 
 runcmd "echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' | sudo tee /etc/apt/sources.list.d/kali.list"
-wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add -
+runcmd "wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add -"
 runcmd "sudo apt update"
 
 
@@ -53,12 +57,13 @@ checkInstalled evilginx2 evilginx2
 checkInstalled unzip unzip
 checkInstalled sqlite3 sqlite3
 checkInstalled ifconfig net-tools
+checkInstalled git git
 
 askInstall "Gophish CLI (by gosecure on Github)" "git clone --recursive https://github.com/gosecure/gophish-cli"
 askInstall "gophish" "wget https://github.com/gophish/gophish/releases/download/v0.12.1/gophish-v0.12.1-linux-64bit.zip"
 askInstall "MailHog (Version 1.0.1)" "wget https://github.com/mailhog/MailHog/releases/download/v1.0.1/MailHog_linux_386"
 
-# In case a readme already exists will cause unzip to hang
+# Bug where if this repository is cloned. Unzip will have conflicting files and hang
 rm README.md
 # Setup files
 runcmd "unzip gophish-*zip"
@@ -91,11 +96,31 @@ printf '{
         }
 }' > config.json
 
+runcmd "mkdir -p /usr/share/evilginx2/phishlets/"
+runcmd "git clone https://github.com/An0nUD4Y/Evilginx2-Phishlets"
+runcmd "mv Evilginx2-Phishlets/* /usr/share/evilginx2/phishlets/"
 
 runcmd "./MailHog_linux_386" &
 sleep 5
 runcmd "./gophish" &
 sleep 5
+
+runcmd "mkdir -p /usr/share/evilginx2/phishlets/"
+runcmd "git clone https://github.com/An0nUD4Y/Evilginx2-Phishlets"
+runcmd "mv Evilginx2-Phishlets/* /usr/share/evilginx2/phishlets/"
+
+echo "Configuring Evilginx2. What is the domain you have?"
+read domain
+
+ip=$(curl https://api.ipify.org 2> /dev/null)
+gophishAPI=$(sqlite3 gophish.db "SELECT api_key FROM users")
+
+# pipe commands into evilginx
+evilconfig "config domain ${domain}
+config ipv4 ${ip}
+config gophish admin_url https://${ip}:3333
+config gophish api_key ${gophishAPI}"
+
 echo "MailHog will be started and the portal will be binded to port 8025."
 grep -i "Please login with the username" ${logfile} | tr -d '"' | awk '{print "Gophish has started with the initial username " $8" and password "$12}'
 
