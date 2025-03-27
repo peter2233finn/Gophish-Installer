11f054eb99c665310685cb86ce40a6da9c237593
Peter Finn <peter123finn@gmail.com>
2025-03-27 16:56:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ function evilconfig {
 echo "Logs for all actions taken by this script are logged to ${logfile}"
 
 # Add kali repos
-
+runcmd "sh -c \"echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list\""
 runcmd "echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free' | sudo tee /etc/apt/sources.list.d/kali.list"
 runcmd "wget -q -O - https://archive.kali.org/archive-key.asc | sudo apt-key add -"
 runcmd "sudo apt update"
